8ca92c5ceab5b55773e01aa5022d70fd691c788f
Georgios <georgios.p.kondylis@gmail.com>
2025-01-31 01:17:50 +0100

    Added an h2 tag that sais einai poniros

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     <div>
       <h1 className="bg-gray-700 py-4 text-center text-white text-[2rem]">welcome tailwind in installed</h1>
       <h2>o giorgos</h2>
+      <h2>Einai poniros</h2>
     </div>
   );
 }
