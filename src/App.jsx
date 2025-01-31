b6ea03a2a35a453be7211c2998a759814dad9e08
Georgios <georgios.p.kondylis@gmail.com>
2025-01-31 01:22:30 +0100

    added kiauta pou leei mi ta tros

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1 className="bg-gray-700 py-4 text-center text-white text-[2rem]">welcome tailwind in installed</h1>
       <h2>o giorgos</h2>
       <h2>Einai poniros</h2>
+      <h2>kai auta pou lei mi ta tros </h2>
     </div>
   );
 }
