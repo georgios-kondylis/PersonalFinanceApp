9c007352a7fa49227723422ea5177adcbd739a1b
Georgios <georgios.p.kondylis@gmail.com>
2025-01-31 01:15:32 +0100

    initialized a git repo of an new project withought the node_modules i added them in a .gitignore file!

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="bg-gray-700 py-4 text-center text-white text-[2rem]">welcome tailwind in installed</h1>
+      <h2>o giorgos</h2>
+    </div>
+  );
+}
+
+export default App;
