081ed4dce48badc443c309e7950cb16f7fa0ced9
Georgios <georgios.p.kondylis@gmail.com>
2025-02-01 13:16:01 +0100

    added a comment and a pashalis h1 _B2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
       <h1 className="bg-gray-700 py-4 text-center text-white text-[2rem]">welcome tailwind in installed</h1>
       <h2>o giorgos</h2>
       <h2>Einai poniros</h2>
+      {/* auto einai gia to deutero-brach */}
+      <h1>Pashalis Pita pitas</h1>
     </div>
   );
 }
