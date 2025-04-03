e395f8f2bc0901cbcdf49d39a19a6c0bcd6fc091
Georgios <georgios.p.kondylis@gmail.com>
2025-04-03 16:25:06 +0200

    complete

--- src/components/ui/SearchSort.jsx
+++ src/components/ui/SearchSort.jsx
@@ -9,13 +9,13 @@ const SearchSort = ({ handleChangeSelect, selected, setSearchResult}) => {
     <div id="Search_&_Sort" className="flex w-full justify-between gap-[10px]">
 
       <div id="SearchField" className="h-[50px] w-[60%] max-w-[300px] font-sans p-[10px] border hover:border-[black] rounded-[10px] flex items-center">
-        <input className="outline-none h-full flex-1" type="text" name="" id="" placeholder="Search Bills"
+        <input className="outline-none w-full h-full flex-1" type="text" name="" id="" placeholder="Search Bills"
                onChange={(e) => setSearchResult(e.target.value)}/>
         <i className="fa-solid fa-magnifying-glass"></i>
       </div>
 
       <div id="SortField" className="min-w-[130px] w-[170px] flex items-center gap-[10px] font-sans text-[#868686]">
-        <p className="text-nowrap">Sort by</p> 
+        <p className="max-sm:hidden text-nowrap">Sort by</p> 
         <FormControl fullWidth sx={{ height: "50px" }}>
           <Select labelId="" id="" value={selected === ''? 'Latest' : selected} onChange={handleChangeSelect}
             sx={{
